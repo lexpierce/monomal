c306aea8abd541b7df90b0a6d2e46aae8b8b1e70
Alexander Sisco <alex@lexpierce.com>
2026-04-13 16:22:18 -0700

    Updated to mnml 2.8.11

--- static/js/copybutton.js
+++ static/js/copybutton.js
@@ -1,17 +1,56 @@
 (function () {
+	function makeSVG(attrs, children) {
+		var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+		for (var k in attrs) svg.setAttribute(k, attrs[k]);
+		children.forEach(function (child) {
+			var el = document.createElementNS('http://www.w3.org/2000/svg', child.tag);
+			for (var k in child.attrs) el.setAttribute(k, child.attrs[k]);
+			svg.appendChild(el);
+		});
+		return svg;
+	}
+
+	var sharedSVGAttrs = {
+		width: '14', height: '14', viewBox: '0 0 24 24',
+		fill: 'none', stroke: 'currentColor',
+		'stroke-linecap': 'round', 'stroke-linejoin': 'round',
+		'aria-hidden': 'true'
+	};
+
+	function makeCopyIcon() {
+		return makeSVG(Object.assign({}, sharedSVGAttrs, { 'stroke-width': '2' }), [
+			{ tag: 'rect', attrs: { x: '9', y: '9', width: '13', height: '13', rx: '2', ry: '2' } },
+			{ tag: 'path', attrs: { d: 'M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1' } }
+		]);
+	}
+
+	function makeCopiedIcon() {
+		return makeSVG(Object.assign({}, sharedSVGAttrs, { 'stroke-width': '2.5' }), [
+			{ tag: 'polyline', attrs: { points: '20 6 9 17 4 12' } }
+		]);
+	}
+
 	function addCopyButtonToCodeBlocks() {
 		var snippets = document.getElementsByClassName('highlight');
 		for (var i = 0; i < snippets.length; i++) {
-			var button = document.createElement("button");
-			button.innerText = "Copy";
-			button.addEventListener("click", function () {
+			var button = document.createElement('button');
+			button.appendChild(makeCopyIcon());
+			button.setAttribute('aria-label', 'Copy code');
+			button.addEventListener('click', function () {
 				var code = this.previousElementSibling.innerText;
 				navigator.clipboard.writeText(code);
-				this.innerText = 'Copied!';
-				setTimeout(() => { this.innerText = 'Copy'; }, 1000);
+				this.replaceChildren(makeCopiedIcon());
+				this.classList.add('copied');
+				this.setAttribute('aria-label', 'Copied!');
+				var self = this;
+				setTimeout(function () {
+					self.replaceChildren(makeCopyIcon());
+					self.classList.remove('copied');
+					self.setAttribute('aria-label', 'Copy code');
+				}, 2000);
 			});
 			snippets[i].appendChild(button);
 		}
 	}
-	document.addEventListener("DOMContentLoaded", addCopyButtonToCodeBlocks);
+	document.addEventListener('DOMContentLoaded', addCopyButtonToCodeBlocks);
 })();
--- static/js/menu.js
+++ static/js/menu.js
@@ -1,13 +1,18 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
+
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
+
 function mobileMenu() {
 	hamburger.classList.toggle("active");
 	navMenu.classList.toggle("active");
+	document.body.classList.toggle("menu-open");
 }
+
 function closeMenu() {
 	hamburger.classList.remove("active");
 	navMenu.classList.remove("active");
+	document.body.classList.remove("menu-open");
 }
